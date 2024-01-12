964aa5c8b1ccd6e7b5bbae1dc80d1a5b65c543d3
faxriddin2007 <faxriddinfaxriddinov5gmail.com>
2024-01-12 17:56:13 +0300

    change env

--- data/config.py
+++ data/config.py
@@ -6,11 +6,11 @@ env.read_env()
 BOT_TOKEN = env.str("BOT_TOKEN")
 ADMINS = env.list("ADMINS")
 
-DB_NAME = env.list("DB_NAME")
-DB_PASS = env.list("DB_PASS")
-DB_HOST = env.list("DB_HOST")
-DB_PORT= env.list("DB_PORT")
-DB_USER = env.list("DB_USER")
+DB_NAME = env.str("DB_NAME")
+DB_PORT = env.str("DB_PORT")
+DB_HOST = env.str("DB_HOST")
+DB_PASS = env.str("DB_PASS")
+DB_USER = env.str("DB_USER")
 
 channels = [
     (-1001926837172, 'Test kanal', 'https://t.me/Faxriddinov_Backend'),
